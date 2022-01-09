fced0fec795c2b7af34f58034897aadd75de3dde
Zenky <bzenky@gmail.com>
2022-01-09 11:25:17 -0300

    mirage/react-query config, pagination, prefetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,39 +1,34 @@
-import Link from 'next/Link'
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Thead, Td, Th, Tr, Tbody, Text, useBreakpointValue } from "@chakra-ui/react";
+import NextLink from 'next/Link'
+import { useState } from 'react'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Link, Table, Thead, Td, Th, Tr, Tbody, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
+import { useMutation } from 'react-query'
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from '../../services/hooks/useUsers';
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('https://localhost:3000/api/users')
-    const data = await response.json()
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users
-  }, {
-    staleTime: 1000 * 5,
-  })
+  const [page, setPage] = useState(1)
+  const { data, isFetching, isLoading, error } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutes
+    })
+  }
 
   return (
     <Box>
@@ -44,8 +39,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
-            <Link href="/users/create" passHref>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -55,7 +53,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           { isLoading ? (
@@ -80,7 +78,7 @@ export default function UserList() {
                 </Tr>
               </Thead>
               <Tbody>
-                {data.map(user => {
+                {data.users.map(user => {
                   return (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
@@ -88,7 +86,9 @@ export default function UserList() {
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{user.name}</Text>
+                          <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </Link>
                           <Text fontSize="sm" color="gray.300" >{user.email}</Text>
                         </Box>
                       </Td>
@@ -113,7 +113,11 @@ export default function UserList() {
               </Tbody>
             </Table>
 
-            <Pagination />
+            <Pagination 
+              totalCountOfRegisters={data.totalCount}
+              currentPage={page}
+              onPageChange={setPage}
+            />
           </>
           )}
         </Box>
